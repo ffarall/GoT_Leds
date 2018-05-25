b5449468bb9f6a6991ba510e97afa47430bbf335
Facundo <ffarall@itba.edu.ar>
2018-05-25 20:07:04 -0300

    ESTE COMMIT FUNCIONA LO BASICO

--- GoT_Leds/graphic_lib.c
+++ GoT_Leds/graphic_lib.c
@@ -1,22 +1,5 @@
 #include "graphic_lib.h"
 
-void image_to_display(ALLEGRO_BITMAP *bitmap, const char* filename, float dx, float dy, int flags) {
-    bitmap = al_load_bitmap(filename);
-    if(!bitmap) {
-        fprintf(stderr, "failed to load image !\n");
-    }
-    
-    al_draw_bitmap(bitmap, dx, dy, flags);
-    al_flip_display();
-}
-
-icon_t init_icon(float dx, float dy, int flags) {
-    icon_t icon;
-    icon.coordinates[0] = dx;
-    icon.coordinates[1] = dy;
-    icon.flags = flags;
-}
-
 int refresh_icons(ALLEGRO_DISPLAY* display, icon_t * icon)
  {
    icon_t * current_icon = icon;
